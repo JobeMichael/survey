2d9ae394a68b8002a724d5537f2de6236b97c47e
Jobe Michael <jobumichael@gmail.com>
2020-03-04 12:32:00 +0100

    Add card style

--- src/components/SurveyCard/SurveyCard.js
+++ src/components/SurveyCard/SurveyCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./SurveyCard.scss";
+import { Card } from "antd";
 
-const SurveyCard = ({ id, title }) => {
+const SurveyCard = ({ id, title, tagline }) => {
   return (
-    <Link className="card link" to={`/survey/${id}`}>
-      {title}
+    <Link to={`/survey/${id}`}>
+      <Card
+        className="link"
+        title={title}
+        bordered={false}
+        style={{ width: 300 }}
+      >
+        <p>{tagline}</p>
+      </Card>
     </Link>
   );
 };
