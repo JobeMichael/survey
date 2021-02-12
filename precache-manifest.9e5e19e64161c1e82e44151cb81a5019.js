02a8241fa33508ad7998d420d77ae6a56ba15f4f
Jobe Michael <jobumichael@gmail.com>
2021-02-12 13:53:17 +0100

    Updates

---
+++ precache-manifest.9e5e19e64161c1e82e44151cb81a5019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129afba7d5565787c3dd4d87379216b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264aa033c1b1bcf326f9",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "275c8953368e4ade41c5",
+    "url": "/static/css/main.858ea900.chunk.css"
+  },
+  {
+    "revision": "264aa033c1b1bcf326f9",
+    "url": "/static/js/2.7d55c7fb.chunk.js"
+  },
+  {
+    "revision": "b4efbb196e991232dec6c5962829bd23",
+    "url": "/static/js/2.7d55c7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275c8953368e4ade41c5",
+    "url": "/static/js/main.23ea1479.chunk.js"
+  },
+  {
+    "revision": "2df14dfb6a3fd517498b",
+    "url": "/static/js/runtime-main.9311f605.js"
+  }
+]);
